8b3750fdb9a3daaa5977a540b7765c1206384782
Zachary Yedidia <zyedidia@gmail.com>
2022-02-07 15:50:38 -0800

    Support register context switching

--- sw/libaxum/start.s
+++ sw/libaxum/start.s
@@ -30,23 +30,6 @@ _halt:
 	j _halt
 
 _timer_intr_handler:
-	csrrw x31, mscratch, x31
-	sw x1, 0(x31)
-	sw x5, 4(x31)
-	sw x6, 8(x31)
-	sw x7, 12(x31)
-	sw x10, 16(x31)
-	sw x11, 20(x31)
-	sw x12, 24(x31)
-	sw x13, 28(x31)
-	sw x14, 32(x31)
-	sw x15, 36(x31)
-	sw x16, 40(x31)
-	sw x17, 44(x31)
-	sw x28, 48(x31)
-	sw x29, 52(x31)
-	sw x30, 56(x31)
-
 	la t0, timer_base
 	sw x0, mtime(t0) # clear mtime
 	lw x1, mtimecmp(t0)
@@ -55,23 +38,6 @@ _timer_intr_handler:
 	la t0, _timer_irq_handler
 	lw t1, 0(t0)
 	jalr ra, t1
-
-	lw x1, 0(x31)
-	lw x5, 4(x31)
-	lw x6, 8(x31)
-	lw x7, 12(x31)
-	lw x10, 16(x31)
-	lw x11, 20(x31)
-	lw x12, 24(x31)
-	lw x13, 28(x31)
-	lw x14, 32(x31)
-	lw x15, 36(x31)
-	lw x16, 40(x31)
-	lw x17, 44(x31)
-	lw x28, 48(x31)
-	lw x29, 52(x31)
-	lw x30, 56(x31)
-	csrrw x31, mscratch, x31
 	mret
 
 
