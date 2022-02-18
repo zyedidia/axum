8218cb9bbad0c5c5f9bcfccb654f67769b496cbf
Zachary Yedidia <zyedidia@gmail.com>
2022-02-17 16:16:47 -0800

    blink

--- sw/libaxum/exception.c
+++ sw/libaxum/exception.c
@@ -2,6 +2,43 @@
 #include "timer.h"
 #include "riscv_csr.h"
 
+typedef struct {
+    uint32_t zero;
+    uint32_t ra;
+    uint32_t sp;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+} regs_t;
+
+regs_t exc_regs;
+
 void exception_init() {
     // timer interrupt every 1ms
     timer_init_irq(1000);
@@ -10,6 +47,8 @@ void exception_init() {
     write_csr(mie, (1 << 7) | (1 << 11));
     // enable interrupts
     write_csr(mstatus, (1 << 3));
+
+    exc_regs.sp = 0x104000;
 }
 
 void _empty_exception(unsigned mcause) { (void) mcause; while (1) {} }
@@ -26,7 +65,11 @@ void set_exception_handler(void (*handler)(unsigned)) {
     _exception_handler = handler;
 }
 
-void ctimer_irq_handler() {
+void exception_entry() {
+    _exception_handler(read_csr(mcause));
+}
+
+void timer_irq_entry() {
     timer_clear_irq();
     _timer_irq_handler();
 }
--- sw/libaxum/start.s
+++ sw/libaxum/start.s
@@ -2,68 +2,50 @@
 
 .globl _start
 _start:
-	# load scratch space address into mscratch
-	la t0, _mscratch
-	csrw mscratch, t0
-
-	la sp, 0x104000
+	la t0, _user_regs
+	csrw 0x345, t0
+	la sp, 0x103000
 	call _cstart
 _halt:
 	j _halt
 
-_timer_irq_handler_entry:
-	csrrw x31, mscratch, x31
-	sw x1, 0(x31)
-	sw x5, 4(x31)
-	sw x6, 8(x31)
-	sw x7, 12(x31)
-	sw x10, 16(x31)
-	sw x11, 20(x31)
-	sw x12, 24(x31)
-	sw x13, 28(x31)
-	sw x14, 32(x31)
-	sw x15, 36(x31)
-	sw x16, 40(x31)
-	sw x17, 44(x31)
-	sw x28, 48(x31)
-	sw x29, 52(x31)
-	sw x30, 56(x31)
-
-	call ctimer_irq_handler
-
-	lw x1, 0(x31)
-	lw x5, 4(x31)
-	lw x6, 8(x31)
-	lw x7, 12(x31)
-	lw x10, 16(x31)
-	lw x11, 20(x31)
-	lw x12, 24(x31)
-	lw x13, 28(x31)
-	lw x14, 32(x31)
-	lw x15, 36(x31)
-	lw x16, 40(x31)
-	lw x17, 44(x31)
-	lw x28, 48(x31)
-	lw x29, 52(x31)
-	lw x30, 56(x31)
-	csrrw x31, mscratch, x31
+_exception_entry:
+	addi sp, sp, -4
+	sw s0, 0(sp)
+	la s0, exc_regs
+	csrw 0x345, s0
+	call exception_entry
+	la s0, _user_regs
+	csrw 0x345, s0
+	lw s0, 0(sp)
+	addi sp, sp, 4
 	mret
 
+_timer_irq_entry:
+	addi sp, sp, -4
+	sw s0, 0(sp)
+	la s0, exc_regs
+	csrw 0x345, s0
+	call timer_irq_entry
+	la s0, _user_regs
+	csrw 0x345, s0
+	lw s0, 0(sp)
+	addi sp, sp, 4
+	mret
 
 .section .vectors, "ax"
 .option norvc
-
 	# general exception handler
 	.org 0x0
-	j _halt
+	j _exception_entry
 	
 	# timer interrupt handler
 	.org 0x1c
-	j _timer_irq_handler_entry
+	j _timer_irq_entry
 
 	# reset handler
 	.org 0x80
 	j _start
 
 .data
-_mscratch: .space 60
+_user_regs: .space 128
